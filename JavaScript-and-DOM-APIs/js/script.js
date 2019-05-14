f13ef3913339ec38531605c28b2d0de4eaf5517e
romiBello <bello.romi.g@gmail.com>
2019-05-14 14:26:48 -0300

    new index.html with a stylesheet, a hidden section and 'fade in' animation

---
+++ JavaScript-and-DOM-APIs/js/script.js
@@ -0,0 +1,8 @@
+window.addEventListener("load",showHiddenSection);
+
+function showHiddenSection() {
+    // switch screens
+    let Change = document.querySelector(".hidden");
+    Change.className = "showme";
+}
+
