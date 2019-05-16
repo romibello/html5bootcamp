2d148c729a5f7efbfb1f388182c535d50bfc65ea
romiBello <bello.romi.g@gmail.com>
2019-05-16 10:20:35 -0300

    reusable function that gets a response from a url and evento to the button

--- JavaScript-and-DOM-APIs/js/script.js
+++ JavaScript-and-DOM-APIs/js/script.js
@@ -1,12 +1,35 @@
-window.addEventListener("load",showHiddenSection);
-window.addEventListener("click",showAlert);
+window.addEventListener("load",showHiddenSection);//this event happens when the content of the page is fully-loaded 
 
-function showHiddenSection() {
-    // switch screens
+
+let btn = document.getElementById('event');
+btn.addEventListener("click",chuckNorrisJokes);
+let config = {url: 'http://api.icndb.com/jokes/random' ,};
+
+function showHiddenSection() {// switch clases
     let Change = document.querySelector(".hidden");
     Change.className = "showme";
 }
 
-function showAlert(){
+function showAlert(){ //alert requested in Ej 2.2
     alert("stop");
+}
+
+function chuckNorrisJokes(){//Bring a JSON from API 
+    fetch(config.url)
+    .then(function(r){
+        if(!r.ok){
+            console.log("error");
+        }
+        return r.json()
+    })
+    .then(function(json){//I print the JSON jokes in the section.
+        let contenedor = document.querySelector("#result");
+        contenedor.innerHTML ='';
+        contenedor.innerHTML += json.value.joke;
+    })
+    .catch(function(e){//If a server error occurs section content must turn red
+        console.log(e)
+        let Change = document.querySelector(".hidden");
+        Change.classList.add("WarningShowme");
+    })
 }
