18b1e69c7c7cc614d70d8506fe2767b781c9907d
romiBello <bello.romi.g@gmail.com>
2019-05-29 12:14:30 -0300

    topic4-Ej2: drag and drop, and fixes

--- HTML5-APIs/js/script.js
+++ HTML5-APIs/js/script.js
@@ -1,3 +1,12 @@
+/*****************************************check*****************************************************/
+window.indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB;
+
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+  //alert("Great success! All the File APIs are supported.");
+} else {
+  alert('The File APIs are not fully supported in this browser.');
+}
+/*****************************************check*****************************************************/
 /*****************************************variables*****************************************************/
 
 let text = document.getElementById("textArea");
@@ -13,6 +22,10 @@ clear.addEventListener("click" , deleteText);
 
 function saveText() {
   window.localStorage.setItem('Info', text.value);
+  if(file){
+    let val = File.value;
+    alert("valooor: " + val);
+  }
   addDBElement();
 }
 
@@ -43,5 +56,36 @@ function removeDBElement(){
   let objectStore = db.transaction("Topic4DB","readwrite").objectStore("Topic4DB");
   objectStore.clear();
 }
- 
+
+/******************************************* Drag and drop *********************************************/
+
+var dropZone = document.getElementById('drop_zone');
+dropZone.addEventListener('dragover', handleDragOver, false);
+dropZone.addEventListener('drop', handleFileSelect, false);
+
+function handleFileSelect(evt) {
+  evt.stopPropagation();
+  evt.preventDefault();
+
+  var files = evt.dataTransfer.files; // FileList object.
+
+  // files is a FileList of File objects. List some properties.
+  var output = [];
+  var reader = new FileReader();
+  let read = files[0];
+  text.value = '';
+  reader.readAsText(read);
+  reader.onloadend = function() {
+    text.value = reader.result;    
+  }
+
+}
+
+function handleDragOver(evt) {
+  evt.stopPropagation();
+  evt.preventDefault();
+  evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+}
+
+
 /*****************************************functions*****************************************************/
